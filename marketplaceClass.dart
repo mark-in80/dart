6299a1cc43ae0cbf3dec092cb8cc1d574cda7b70
mark-lopin101 <neurocore2sovka@gmail.com>
2023-06-07 20:59:10 +0300

    modified addNewProduct method: add enter product for users

--- marketplaceClass.dart
+++ marketplaceClass.dart
@@ -4,7 +4,8 @@ void main() {
   Marketplace marketplace =
       Marketplace("Prosrochka", 10000, "Uncle Vasia", 100500, [], []);
   Product product = Product(0, 0, "_category");
-  product.addNewProduct(marketplace);
+  productsMethod(marketplace);
+  addNewProduct(marketplace);
   marketplace.showProducts();
 }
 
@@ -12,13 +13,41 @@ void staffMethod(Marketplace marketplace) {
   marketplace.staffs.add(Staff("Ivan", "Ivanov", _JobTitle.admin, 40000));
   marketplace.staffs.add(Staff("Petya", "Petrov", _JobTitle.cashier, 30000));
   marketplace.staffs.add(Staff("Vova", "Sidorov", _JobTitle.cook, 35000));
-
 }
 
 void productsMethod(Marketplace marketplace) {
   marketplace.products.add(Product(150, 0.1, "candy"));
   marketplace.products.add(Product(10, 1, "bread"));
   marketplace.products.add(Product(5, 1, "bubble"));
+}
+
+void addNewProduct(Marketplace marketplace) {
+  var abstractVariable = " ";
+
+  while (abstractVariable != "break") {
+    stdout.write("Enter  Name products,  or Enter break for stop: ");
+    try {
+      String nameProduct = stdin.readLineSync() ?? '0';
+      if (nameProduct == "break") {
+        break;
+      }
+      stdout.write("Enter  Price,  or Enter break for stop: ");
+      double priceProduct = double.parse(stdin.readLineSync() ?? '0');
+      if (priceProduct == "break") {
+        break;
+      }
+      stdout.write("Enter  Count,  or Enter break for stop: ");
+      double countProduct = double.parse(stdin.readLineSync() ?? '0');
+      if (countProduct == "break") {
+        break;
+      } else {
+        marketplace.products
+            .add(Product(priceProduct, countProduct, nameProduct));
+      }
+    } catch (exception) {
+      print(exception);
+    }
+  }
 }
 
 enum _JobTitle { cook, admin, cashier }
@@ -94,44 +123,15 @@ abstract class FormatProducts {
 }
 
 class Product<T> implements FormatProducts {
-  final T _nameProduct;
-  final T _price;
-  final T _count;
+  final String _nameProduct;
+  final double _price;
+  final double _count;
 
   Product(this._price, this._count, this._nameProduct);
 
   @override
   String get productsFormat =>
       "Category: $_nameProduct; price: $_price; count: $_count";
-
-  void addNewProduct(Marketplace marketplace) {
-    var abstractVariable = " ";
-
-    while (abstractVariable != "break") {
-      stdout.write("Enter  Name products,  or Enter break for stop: ");
-      try {
-        T nameProduct = stdin.readLineSync() as T;
-        if (nameProduct == "break") {
-          break;
-        }
-        stdout.write("Enter  Price,  or Enter break for stop: ");
-        T priceProduct = stdin.readLineSync() as T;
-        if (priceProduct == "break") {
-          break;
-        }
-        stdout.write("Enter  Count,  or Enter break for stop: ");
-        T countProduct = stdin.readLineSync() as T;
-        if (countProduct == "break") {
-          break;
-        } else {
-          marketplace.products
-              .add(Product(priceProduct, countProduct, nameProduct));
-        }
-      } catch (exception) {
-        print(exception);
-      }
-    }
-  }
 }
 
 class Tax {
